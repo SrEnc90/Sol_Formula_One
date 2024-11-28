14d35fa192dbfeae69bd5cd52186aff96a02d732
SrEnc90 <carl.enc90@gmail.com>
2024-11-28 13:49:22 -0500

    services blazor

--- FormulaOne.Entities/Dtos/Reponses/GetDriverResponse.cs
+++ FormulaOne.Entities/Dtos/Reponses/GetDriverResponse.cs
@@ -4,6 +4,8 @@ public class GetDriverResponse
 {
     public Guid DriverId { get; set; }
     public string FullName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public int DriverNumber { get; set; }
     public DateTime DateOfBirth { get; set; }
 }
--- FormulaOne.Service/obj/Debug/net6.0/FormulaOne.Service.AssemblyInfo.cs
+++ FormulaOne.Service/obj/Debug/net6.0/FormulaOne.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormulaOne.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976514cedb2fd6bb56a4dc22ef8dfa447b81ae81")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormulaOne.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormulaOne.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
